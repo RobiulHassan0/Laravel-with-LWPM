94c6da3b88f4018ae01c04ce82982fd104446273
RobiulHassan0 <robiulhassan270@gmail.com>
2026-02-07 04:05:53 +0600

    laravel custom authentication done

--- authenticate_laravel/routes/web.php
+++ authenticate_laravel/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\HomeController;
@@ -14,6 +15,8 @@ Route::post('/register', [RegisterController::class, 'store']);
 
 Route::get('/login', [LoginController::class, 'index'])->name('login.form');
 Route::post('/login', [LoginController::class, 'store'])->name('login');
+
+Route::post('/logout', LogoutController::class)->name('logout');
 
 // Route::group(['middleware' => 'auth'], function () {
 //     Route::get('/dashboard', DashboardController::class)->name('dashboard');
